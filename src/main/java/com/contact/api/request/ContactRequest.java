e2496a0e48302221deaeccc2bf97e309a88afec5
Srishti Harit <86703642+haritsrishti@users.noreply.github.com>
2024-04-19 01:32:12 +0530

    Added contactRequest and application details

---
+++ src/main/java/com/contact/api/request/ContactRequest.java
@@ -0,0 +1,12 @@
+package com.contact.api.request;
+
+public class ContactRequest{
+
+	public int id;
+	public String name;
+	public String phone;
+	public String email;
+
+
+}
+
---
+++ src/main/java/com/contact/api/request/Contacts.java
@@ -0,0 +1,9 @@
+package com.contact.api.request;
+
+import java.util.List;
+
+public class Contacts{
+
+	List<ContactRequest> contacts;
+	
+}
